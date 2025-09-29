bf6aaca738b2109508719d797bcc4e567aac3f62
hbnrkrdgn7 <hkaradogan4455@gmail.com>
2025-09-29 11:52:01 +0300

    link commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -14,14 +14,14 @@ const Navbar = () => {
 </div>
 
 
-
       {/* Menü Linkleri */}
       <ul className="navbar-links">
-        <li><a href="#about">Hakkımızda</a></li>
-        <li><a href="#jury">Jüri - Yarışma Yazılımı</a></li>
-        <li><a href="#word-ninja">Word Ninja</a></li>
-        <li><a href="#word-pyramids">Word Pyramids</a></li>
+        <li><a href="https://rastmobile.com/en/about" target="_blank" rel="noopener noreferrer">Hakkımızda</a></li>
+        <li><a href="https://rastmobile.com/en/products/jury-competition-software" target="_blank" rel="noopener noreferrer">Jüri - Yarışma Yazılımı</a></li>
+        <li><a href="https://rastmobile.com/en/products/word-ninja" target="_blank" rel="noopener noreferrer">Word Ninja</a></li>
+        <li><a href="https://rastmobile.com/en/products/word-pyramids" target="_blank" rel="noopener noreferrer">Word Pyramids</a></li>
       </ul>
+
 
       {/* Sosyal Medya İkonları */}
       <div className="navbar-icons">
